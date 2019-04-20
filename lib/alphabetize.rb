6c8d223c7a795a9420e5b4d7ede97cc5f1c5a77d
Laura Hoffman <lhoffmanwg@gmail.com>
2019-04-20 23:40:03 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,8 @@ ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(sentences_array)
   sentences_array.sort_by do |sentence|
     sentence.split("").map do |character|
-      ALPHABET.index(character)
+      me=ALPHABET.index(character)
+      binding.pry
     end
   end
 end
